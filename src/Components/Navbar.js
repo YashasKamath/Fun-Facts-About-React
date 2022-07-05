911712d7005aea0d3ac12ac4054b8ecf80db6e1a
Yashas <yashaskamath81@gmail.com>
2022-07-05 19:43:21 +0530

    added dark/light mode switch

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,33 @@
-import React from "react"
+import React,{useEffect, useState} from "react"
 import vector from '../images/Vector.png'
 
 export default function Navbar(){
+    const [checkbox,setChecked]=useState(true)
+
+    function handleChange(){
+        setChecked(prevState=>!prevState)
+    }
+    
+    useEffect(()=>{
+        let body=document.body
+        if(checkbox) body.className='dark-mode'
+        else body.className='light-mode'
+    },[checkbox])
     return(
         <nav className="nav">
-            <img src={vector} className="circle"/>
+            <img src={vector} className="circle" alt='react logo'/>
             <h3 className="reactFacts">ReactFacts</h3>
-            <h4 className="project">React Course - Project 1</h4>
+            <p className="light">Light</p>
+            <label class="switch">
+                <input 
+                    type="checkbox" 
+                    checked={checkbox}
+                    onChange={handleChange}
+                    id="checkbox"
+                />
+                <span class="slider round"></span>
+            </label>
+            <p  className="dark">Dark</p>
         </nav>
     )
 }
